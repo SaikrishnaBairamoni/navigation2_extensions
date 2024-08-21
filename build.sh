e4ced27f86e2655fdfd0f19bddf295bf24e48dc7
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-08-21 16:13:07 -0400

    try

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 /__w/navigation2_extensions/navigation2_extensions
-source /opt/ros/humble/setup.sh
-# Build 
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+# source /opt/ros/humble/setup.sh
+# # Build 
+# colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+. /opt/ros/humble/setup.sh && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
