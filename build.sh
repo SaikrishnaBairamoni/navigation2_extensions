a834325c24a28bb74cd553a3b6a30d9671a8286f
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-08-21 16:17:32 -0400

    test

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-/__w/navigation2_extensions/navigation2_extensions
-# source /opt/ros/humble/setup.sh
+source /opt/ros/humble/setup.sh
 # # Build 
 # colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
-. /opt/ros/humble/setup.sh && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+colcon build --packages-select carma_nav2_behavior_tree carma_nav2_emergency_stop carma_nav2_port_drayage_demo
