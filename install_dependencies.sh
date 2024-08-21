983fa4ffef22120ec224233d23c5d40708177185
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-08-21 14:01:44 -0400

    try

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e 
 
-apt update
-
-# Install 
+apt update 
 apt install -y libnanoflann-dev
-
 # Initialize rosdep
-rosdep init
-rosdep update
 rosdep install --from-paths /root/c1t_ws/src --ignore-src -y
