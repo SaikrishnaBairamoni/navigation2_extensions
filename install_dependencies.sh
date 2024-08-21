3059384fa3454c9e07224af2974db2cff1e603b6
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-08-21 14:59:53 -0400

    try

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,6 +6,4 @@ cd /__w/navigation2_extensions/navigation2_extensions
 # Install 
 apt install -y libnanoflann-dev
 
-# Initialize rosdep
-rosdep update
 rosdep install --from-paths /__w/navigation2_extensions/navigation2_extensions/src --ignore-src -y
